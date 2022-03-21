d2c7b0e86448106812c24c1e341ae5305a5408d6
nagolove <dekar.own@gmail.com>
2022-03-21 17:28:41 +0300

    simplest guard checker.

--- src/main.c
+++ src/main.c
@@ -11,53 +11,61 @@
 
 const char *guard_def = "abcdefg";
 
+typedef struct {
+    struct GuardedBlock *next;
+    void *ptr;
+    size_t size;
+} GuardedBlock;
+
+GuardedBlock *guarded_blocks_root = NULL;
+
 void *new_guarded_user_data(lua_State *lua, size_t size) {
     size_t full_size = 
         size + 
-        strlen(guard_def) * 2 + // длина хранителей
+        strlen(guard_def) * 2 + // длина строчек хранителей
         2 +                     // два терминальных нуля
         sizeof(size_t);         // для хранения размера выделенного куска
-    void *ud = lua_newuserdata(lua, full_size);
-    memset(ud, 0, full_size);
+    char *ptr = lua_newuserdata(lua, full_size);
+    memset(ptr, 0, full_size);
 
-    //char *ret = strcpy((char*)ud, guard_def);
-    //*++ret = 0;
-    //*(size_t*)ret = size;
-    //ret += sizeof(size);
-    //char *last = (char*)ud + strlen(guard_def) + size + 1;
-    //ret = strcpy(last, guard_def);
-    //*++ret = 0;
+    ptr += sprintf(ptr, guard_def) + 1;
+    *(size_t*)ptr = size;
+    ptr += sizeof(size_t);
+    void *ret = ptr;
+    ptr += size;
+    sprintf(ptr, guard_def);
 
-    char *ret = strcpy((char*)ud, guard_def);
-    *++ret = 0;
-    ret += sizeof(size_t);
-    *(size_t*)ret = size;
-
-    ret += size;
-    char *last = ret;
-    /*char *last = (char*)ud + strlen(guard_def) + size + 1;*/
-
-    ret = strcpy(last, guard_def);
-    *++ret = 0;
-
-    return (char*)ud + strlen(guard_def) + 1 + sizeof(size_t);
+    return ret;
 }
 
 bool check_guarded_user_data(void *ud) {
-    char *first = (char*)ud - strlen(guard_def) - 1 - sizeof(size_t);
-    /*char *second = (char*)ud;*/
-    printf("first %s\n", first);
+    char *ptr = ud;
+    char *first = ptr - strlen(guard_def) - 1 - sizeof(size_t);
 
-    return false;
+    // Превентивная установка конечного нуля на строчку если он был затерт.
+    char *null_zero = ptr - sizeof(size_t) - 1;
+    *null_zero = 0;
+
+    size_t size = *(size_t*)(ptr - strlen(guard_def) - 1);
+    char *second = ptr + size;
+    
+    /*printf("fir '%s'\n", first);*/
+    /*printf("sec '%s'\n", second);*/
+    /*printf("size %d\n", (int)size);*/
+
+    return !strcmp(first, second);
 }
 
 int main(void) {
     malloc(12);
     lua_State *lua = luaL_newstate();
 
-    void *ud = new_guarded_user_data(lua, 100);
-    strcpy((char*)ud, "strokovui_literal_mmm");
-    check_guarded_user_data(ud);
+    size_t sz = 101;
+    void *ud = new_guarded_user_data(lua, sz);
+    /*strcpy((char*)ud, "strokovui_literal_mmm");*/
+    memset(ud, 'z', sz);
+
+    printf("check %d\n", (int)check_guarded_user_data(ud));
 
     lua_close(lua);
 
